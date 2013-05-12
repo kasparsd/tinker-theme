86e91bdb269eaf80bf2083e0e4c6615496156a1e
Kaspars <kaspars@konstruktors.com>
2013-05-12 20:29:54 +0200

    Move theme setup out of action

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -1,29 +1,27 @@
 <?php
 
 
-add_action( 'after_theme_setup', 'tinker_setup' );
+// Enable post thumbnails
+add_theme_support( 'post-thumbnails' );
 
-function tinker_setup() {
-	// Enable post thumbnails
-	add_theme_support( 'post-thumbnails' );
+// Add an image size for featured images
+add_image_size( 'featured-header', 800, 400, true );
 
-	// Add an image size for featured images
-	add_image_size( 'featured-header', 800, 400, true );
 
-	// Register menus
-	register_nav_menu( 'main_menu', 'Main Menu' );
-	register_nav_menu( 'footer_menu', 'Footer Menu' );
+// Register menus
+register_nav_menu( 'main_menu', 'Main Menu' );
+register_nav_menu( 'footer_menu', 'Footer Menu' );
 
-	// Register the sidebar
-	register_sidebar( array(
-		'id' => 'footer-area',
-		'name' => __( 'Footer' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<h1 class="widget-title">',
-		'after_title' => '</h1>',
-	) );
-}
+
+// Register the sidebar
+register_sidebar( array(
+	'id' => 'footer-area',
+	'name' => __( 'Footer' ),
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => '</aside>',
+	'before_title' => '<h1 class="widget-title">',
+	'after_title' => '</h1>',
+) );
 
 
 add_action( 'wp_enqueue_scripts', 'tinker_child_css' );
