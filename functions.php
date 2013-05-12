6157544b9f0dee460c6e1bc150e9992bc3cf2aec
Kaspars <kaspars@konstruktors.com>
2013-05-12 21:38:24 +0200

    Add i10n

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
+ * Enable localization
+ */
+load_theme_textdomain( 'tinker', get_template_directory() . '/languages' );
+
+
+/**
  * Include all files inside the inc folder
  */
 foreach ( glob( dirname( __FILE__ ) . '/inc/*.php' ) as $include_file )
