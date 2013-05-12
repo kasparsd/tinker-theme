7621852544f1eb59e8ea7d0be46f2a0a8169d828
Kaspars <kaspars@konstruktors.com>
2013-05-12 20:28:58 +0200

    Comment the include procedure

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
-foreach ( glob( 'inc/*.php' ) as $include_file )
+/**
+ * Include all files inside the inc folder
+ */
+foreach ( glob( dirname( __FILE__ ) . '/inc/*.php' ) as $include_file )
 	include $include_file;
 
