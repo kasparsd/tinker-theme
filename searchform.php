debfa3ed1e3f33df00ab08047c9c24d261647ed7
Kaspars <kaspars@konstruktors.com>
2013-05-14 11:43:12 +0200

    Add custom search form with placeholder

---
+++ searchform.php
@@ -0,0 +1,6 @@
+
+<form method="get" id="searchform" action="<?php echo home_url(); ?>" role="search">
+	<label for="s" class="assistive-text"><?php _e( 'Search', 'tinker' ); ?></label>
+	<input type="text" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'tinker' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" />
+	<input type="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'tinker' ); ?>" />
+</form>
