052dcb670dfd3b359249701ebd906af190a63383
Kaspars <kaspars@konstruktors.com>
2013-07-07 15:15:29 +0200

    Move theme setup into own hook

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -1,27 +1,48 @@
 <?php
 
 
-// Enable post thumbnails
-add_theme_support( 'post-thumbnails' );
+add_action( 'after_setup_theme', 'tinker_theme_setup' );
 
-// Add an image size for featured images
-add_image_size( 'featured-header', 800, 400, true );
+function tinker_theme_setup() {
+	global $content_width;
 
+	// Enable localization
+	load_theme_textdomain( 'tinker', get_template_directory() . '/languages' );
 
-// Register menus
-register_nav_menu( 'main_menu', 'Main Menu' );
-register_nav_menu( 'footer_menu', 'Footer Menu' );
+	// Required only for theme check, not used in theme
+	if ( ! isset( $content_width ) ) 
+		$content_width = 650;
 
+	// Support custom background colors and images
+	add_theme_support( 'custom-background', array( 'default-color' => 'eeeeee' ) );
 
-// Register the sidebar
-register_sidebar( array(
-	'id' => 'footer-area',
-	'name' => __( 'Footer Widget Area', 'tinker' ),
-	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-	'after_widget' => '</aside>',
-	'before_title' => '<h1 class="widget-title">',
-	'after_title' => '</h1>',
-) );
+	// Add RSS links, etc.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Enable post thumbnails
+	add_theme_support( 'post-thumbnails' );
+
+	// Add an image size for featured images
+	add_image_size( 'featured-header', 800, 400, true );
+
+	// Register menus
+	register_nav_menu( 'main_menu', 'Main Menu' );
+	register_nav_menu( 'footer_menu', 'Footer Menu' );
+
+	// Register the sidebar
+	register_sidebar( array(
+		'id' => 'footer-area',
+		'name' => __( 'Footer Widget Area', 'tinker' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h1 class="widget-title">',
+		'after_title' => '</h1>',
+	) );
+
+	if ( is_singular() ) 
+		wp_enqueue_script( 'comment-reply' );
+
+}
 
 
 add_action( 'wp_enqueue_scripts', 'tinker_base_css' );
