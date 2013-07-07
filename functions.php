098b4e6cfa18f8b0d3f06b792a0c8b8a593c5f54
Kaspars <kaspars@konstruktors.com>
2013-07-07 15:14:43 +0200

    Hardcode theme includes files

--- functions.php
+++ functions.php
@@ -1,14 +1,11 @@
 <?php
 
-/**
- * Enable localization
- */
-load_theme_textdomain( 'tinker', get_template_directory() . '/languages' );
+include 'inc/theme-setup.php';
 
+include 'inc/theme-parts.php';
 
-/**
- * Include all files inside the inc folder
- */
-foreach ( glob( dirname( __FILE__ ) . '/inc/*.php' ) as $include_file )
-	include $include_file;
+include 'inc/theme-menus.php';
 
+include 'inc/theme-features.php';
+
+include 'inc/theme-admin.php';
