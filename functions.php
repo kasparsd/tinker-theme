9e1685948be58c925ae85259e3433e02526d0acc
Kaspars <kaspars@konstruktors.com>
2013-09-24 16:49:59 +0300

    Add file comments

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
 <?php
 
+// After theme setup, basic enqueues, theme properties
 include 'inc/theme-setup.php';
 
+// Theme elements, post meta data, pagination
 include 'inc/theme-parts.php';
 
+// Only menus
 include 'inc/theme-menus.php';
 
+// Nice to have
 include 'inc/theme-features.php';
 
+// Theme options
 include 'inc/theme-admin.php';
