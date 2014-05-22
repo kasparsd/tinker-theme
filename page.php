255cf602c1874570f7f4e0b512b56e695f737bab
Kaspars <hi@kaspars.net>
2014-05-22 11:18:38 +0300

    Replicate the single template

--- page.php
+++ page.php
@@ -1,4 +1,35 @@
 <?php 
 
-// Re-use the single.php template
-get_template_part( 'single' );
+get_header(); 
+
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	<?php do_action( 'post_before' ) ?>
+
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>" role="article">
+
+		<?php do_action( 'post_header' ) ?>
+		
+		<?php if ( get_the_title() ) : ?>
+		<h1 class="entry-title" role="heading">
+			<?php the_title(); ?>
+		</h1>
+		<?php endif; ?>
+
+		<?php do_action( 'after_post_title' ) ?>
+
+		<div class="entry-content">
+			<?php get_template_part( 'content', get_post_type() );  ?>
+		</div>
+		
+		<?php do_action( 'post_footer' ); ?>
+	
+	</article>
+
+	<?php do_action( 'post_after' ) ?>
+
+	<?php comments_template( false, true ); ?>
+
+<?php endwhile; endif;
+
+get_footer();
